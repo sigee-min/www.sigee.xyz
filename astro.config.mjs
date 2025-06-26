612d078089c501fc6fa255cea9e9c44405524e55
sigee-min <minshigee@gmail.com>
2025-06-26 14:26:50 +0000

    fix: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sigee-min.github.io/",
-	base: "sigee-min.github.io",
+	site: "https://sigee-min.github.io",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
