7f9f2af33678982330f493f35e7df7be743ea269
sigee-min <minshigee@gmail.com>
2025-06-30 15:56:33 +0000

    url

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.sigee.syz",
+	site: "https://www.sigee.xyz",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
